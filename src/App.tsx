4089717059fd684a22aa0c3e2deeef64076a064f
treva556 <marktreva92@gmail.com>
2024-11-09 16:01:31 +0300

    ret

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,47 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-     
+            HERO
       </header>
-      <div>
+      <div className=' mt-4 h-10'>
+        <p className=' underline'> About  </p>
+      </div>
+      <div className='grid grid-cols-3 mt-4 m-8  gap-5'>
+      <div className=' bg-blue-200 rounded-lg p-6'>
         AAA
       </div>
-      <div>
+      <div className=' bg-blue-200 rounded-lg p-6'>
+        AAA
+      </div>
+      <div className=' bg-blue-200 rounded-lg p-6'>
+        AAA
+      </div>
+      </div>
+
+      <div className='grid grid-cols-2 mt-4  gap-5'>
+      <div className=' bg-blue-500 rounded-lg p-6'>
+        AAA
+      </div>
+      <div className=' bg-blue-200 rounded-lg p-6'>
+        AAA
+      </div>
+     
+      </div>
+
+      {/* footer */}
+      <div className='border border-black mt-2' >
+
+     footer
+      <div className='grid grid-cols-3  '>
         
+      <div className=' p-6'>
+        AAA
+      </div>
+      <div className=' p-6'>
+        AAA
+      </div>
+
+      </div>
       </div>
     </div>
   );
